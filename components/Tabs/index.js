5125285d1146180d7a17bbc9c99e86e3f71f3e49
Elysia Gabe <elysiagabe@gmail.com>
2020-02-14 08:44:46 -0800

    Creates Tabs function and component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// Tab parent div
+const tabContainer = document.querySelector(".topics"); 
+
+// Axios call
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(res => {
+    console.log(res.data);
+    // will pull & append data here
+    tabContainer.appendChild(Tabs(res.data))
+})
+.catch(err => console.log(`Error fetching data. ${err}`))
+
+// function
+function Tabs(obj) {
+    // create element
+    let tabDiv = document.createElement("div");
+
+    // add class
+    tabDiv.classList.add("tab");
+
+    // add content
+    tabDiv.textContent = obj.topics;
+
+    return tabDiv
+}
